9f0e2ff50b43f9f3e94a3578f6a88df3627e411b
nursh <nsheikhmohammed@yahoo.com>
2019-03-02 08:29:44 -0500

    Added About section with styles

---
+++ src/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About: React.FC = () => (
+  <section className="about">
+    <h2 className="section__title">About Me</h2>
+    <p className="about__text">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, sed.
+      Vel nostrum, in placeat unde atque voluptas 
+      consectetur adipisci est facilis sunt repellat explicabo quibusdam eius beatae culpa magnam voluptatibus.
+    </p>
+  </section>
+)
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 
 import { Hero } from './Hero';
+import { About } from './About';
 
 const App: React.FC = () => (
   <div className="container">
     <Hero />
+    <About />
   </div>
 );
 
